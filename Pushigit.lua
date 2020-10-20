8c8074c61448063f1f368b77e247114bbda0960f
Pushi <ajbacc@gmail.com>
2020-10-19 19:14:12 -0500

    bug fix

--- Pushigit.lua
+++ Pushigit.lua
@@ -1,3 +1,4 @@
 local file = arg[1]..".lua"
+local link = "https://raw.githubusercontent.com/Pushigoh/ComputerCraftUtils/main/"..file
 shell.run(rm file)
-shell.run(wget "https://raw.githubusercontent.com/Pushigoh/ComputerCraftUtils/main/"..file file)
+shell.run(wget link file)
