8a8619fd831ca429557f3c00b5fbbefa6fd28787
Pushi <ajbacc@gmail.com>
2020-10-19 21:19:00 -0500

    ChopTree bug fix
    Prevents turtle from endlessly mining up

--- turtleutil.lua
+++ turtleutil.lua
@@ -8,6 +8,10 @@ function ChopTree()
 		turtle.dig()
 		turtle.digUp()
 		turtle.up()
+		a, b = turtle.inspectUp()
+		if b.name ~= "minecraft:oak" and b.name ~= "minecraft:leaves" then
+			Land()
+		end
 	end
 		Land()
 end
